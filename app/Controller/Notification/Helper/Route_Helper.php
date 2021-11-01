b26cd779f06e9a26d8d04244b51c0e75887f8a94
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2021-11-01 11:53:20 +0600

    improved

--- app/Controller/Notification/Helper/Route_Helper.php
+++ app/Controller/Notification/Helper/Route_Helper.php
@@ -3,7 +3,9 @@
 namespace DirectoristAppToolkit\Controller\Notification\Helper;
 
 trait Route_Helper {
-    public $notification_route = 'https://firestore.googleapis.com/v1/projects/directorist-app/databases/(default)/documents/notifications';
+    public static function get_route( $document_id = '' ) {
+        return "https://firestore.googleapis.com/v1/projects/directorist-app/databases/(default)/documents/notifications/{$document_id}/notifications";
+    }
 
     public static function get_route_args( $args ) {
         return [
@@ -21,5 +23,12 @@ trait Route_Helper {
                 // 'Authorization' => 'key=AAAAK1QbsOQ:APA91bGjR5P97RhV3Pom-hOIDUMwrBBQ9Lv5frAYjeVaaqK90O8XO4pUKV8168DekBz-7ffllrJfbhibWh9C1RY00GIwIHrb0pCAk_nEAF3Qt4YLqi80XBBAqaDUADSfmVNuUEKP2VIu',
         ];
     }
-}
 
+    public static function get_topic_id( $email = '' ) {
+        return join( '_', unpack( 'C*', $email ) );
+    }
+
+    public static function get_current_time() {
+        return date(DATE_ISO8601, strtotime( current_time( 'mysql' ) ));
+    }
+}
--- app/Controller/Notification/Message_Notification/Notify_Listing_Owner_For_New_Message.php
+++ app/Controller/Notification/Message_Notification/Notify_Listing_Owner_For_New_Message.php
@@ -4,7 +4,6 @@ namespace DirectoristAppToolkit\Controller\Notification\Message_Notification;
 
 use DirectoristAppToolkit\Controller\Notification\Helper;
 
-
 class Notify_Listing_Owner_For_New_Message {
 
     use Helper\Route_Helper;
@@ -14,29 +13,138 @@ class Notify_Listing_Owner_For_New_Message {
     }
 
     public function send_notification( $action_args = [] ) {
-        $sender_name = ( isset( $action_args['sender_name'] ) ) ? $action_args['sender_name'] : 'someone';
-        $message = ( isset( $_POST["message"] ) ) ? stripslashes(esc_textarea($_POST["message"])) : '';
+        $sender_name   = ( isset( $action_args['sender_name'] ) ) ? $action_args['sender_name'] : 'someone';
+        $receiver_name = 'Someone';
+        $reviewer_name = 'Someone';
+        $message       = ( isset( $_POST["message"] ) ) ? stripslashes(esc_textarea($_POST["message"])) : '';
 
-        $notification_title = "A message from $sender_name is received";
-        $notification_body = $message;
+        $email = ( isset( $action_args['to_email'] ) ) ? $action_args['to_email'] : '';
+        $topic = $this->get_topic_id( $email );
 
-        $message_title = "A message from $sender_name is received";
-        $message_body = $message;
-
-        $fields = [
+        $listing_id    = 37;
+        // $listing_id = ( isset( $action_args['listing_id'] ) ) ? $action_args['listing_id'] : 0;
+        // $listing_title = get_the_title( $listing_id );
+        $listing_title = 'Sun Apartment';
+        
+        $order_id          = 123;
+        $listing_thumbnail = 'https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80';
+        $user_avater_1     = 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80';
+        $user_avater_2     = 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80';
+        
+        $order_notification_title = "Your Order #{$order_id} is Received";
+        $order_fields = [
             "fields" => [
-                "title" => [ "stringValue" => $notification_title ],
-                "body"  => [ "stringValue" => $notification_body ],
-                "data"  => [ "mapValue" => [ "fields" => [
-                    "title" => [ "stringValue" => $message_title ],
-                    "body"  => [ "stringValue" => $message_body ],
+                "title"           => [ "stringValue" => $order_notification_title ],
+                "font_icon"       => [ "stringValue" => 'fas fa-receipt' ],
+                "font_icon_color" => [ "stringValue" => '#F65490' ],
+                "data"            => [ "mapValue" => [ "fields" => [
+                    'type'   => [ "stringValue" => 'orderNotification' ],
+                    'action' => [ "stringValue" => 'navigateToNotificationScreen' ],
                 ]]],
-                // "topic"        => [ "stringValue" => "admin" ],
-                "topic"        => [ "stringValue" => "test" ],
-                "date_created" => [ "stringValue" => current_time( 'mysql' ) ],
+                "topic"        => [ "stringValue" => $topic ],
+                "seen"         => [ "booleanValue" => false ],
+                "date_created" => [ "stringValue" => $this->get_current_time() ],
             ]
         ];
 
-        wp_remote_post( $this->notification_route, self::get_route_args( $fields ) );
+        $listing_notification_title = "Your listing {$listing_title} has been submitted";
+        $listing_fields = [
+            "fields" => [
+                "title"      => [ "stringValue" => $listing_notification_title ],
+                "image"      => [ "stringValue" => $listing_thumbnail ],
+                "image_icon" => [ "stringValue" => $user_avater_1 ],
+                "data"       => [ "mapValue" => [ "fields" => [
+                    'type'       => [ "stringValue" => 'listingNotification' ],
+                    'action'     => [ "stringValue" => 'navigateToSingleListingScreen' ],
+                    'listing_id' => [ "stringValue" => "{$listing_id}" ],
+                ]]],
+                "topic"        => [ "stringValue" => $topic ],
+                "seen"         => [ "booleanValue" => false ],
+                "date_created" => [ "stringValue" => $this->get_current_time() ],
+            ]
+        ];
+
+        $message_notification_title = "A message from $sender_name is received";
+        $message_fields = [
+            "fields" => [
+                "title"      => [ "stringValue" => $message_notification_title ],
+                "image_icon" => [ "stringValue" => $user_avater_2 ],
+                "data"       => [ "mapValue" => [ "fields" => [
+                    'type'                 => [ "stringValue" => 'messageNotificaton' ],
+                    'action'               => [ "stringValue" => 'navigateToMessageScreen' ],
+                    'from_user_id'         => [ "stringValue" => "1" ],
+                    // 'from_user_avater_src' => [ "stringValue" => $user_avater_2 ],
+                    'listing_id'           => [ "stringValue" => "{$listing_id}" ],
+                    'message'              => [ "stringValue" => $message ],
+                ]]],
+                "topic"        => [ "stringValue" => $topic ],
+                "seen"         => [ "booleanValue" => false ],
+                "date_created" => [ "stringValue" => $this->get_current_time() ],
+            ]
+        ];
+
+        $group_message_notification_title = "$sender_name has sent a message to $receiver_name";
+        $group_message_fields = [
+            "fields" => [
+                "title"           => [ "stringValue" => $group_message_notification_title ],
+                "font_icon"       => [ "stringValue" => 'fas fa-user-friends' ],
+                "font_icon_color" => [ "stringValue" => '#8C78FE' ],
+                "data"            => [ "mapValue" => [ "fields" => [
+                    'type'                 => [ "stringValue" => 'messageNotificaton' ],
+                    'action'               => [ "stringValue" => 'navigateToMessageScreen' ],
+                    'from_user_id'         => [ "stringValue" => "1" ],
+                    // 'from_user_avater_src' => [ "stringValue" => $user_avater_1 ],
+                    'to_user_id'           => [ "stringValue" => "1" ],
+                    // 'to_user_avater_src'   => [ "stringValue" => $user_avater_2 ],
+                    'listing_id'           => [ "stringValue" => "{$listing_id}" ],
+                    'message'              => [ "stringValue" => $message ],
+                ]]],
+                "topic"        => [ "stringValue" => $topic ],
+                "seen"         => [ "booleanValue" => false ],
+                "date_created" => [ "stringValue" => $this->get_current_time() ],
+            ]
+        ];
+
+        $review_notification_title = "$reviewer_name reviewed on {$listing_title}";
+        $review_fields = [
+            "fields" => [
+                "title"      => [ "stringValue" => $review_notification_title ],
+                "image"      => [ "stringValue" => $listing_thumbnail ],
+                "image_icon" => [ "stringValue" => $user_avater_1 ],
+                "data"       => [ "mapValue" => [ "fields" => [
+                    'type'       => [ "stringValue" => 'reviewNotification' ],
+                    'action'     => [ "stringValue" => 'navigateToSingleListingScreen' ],
+                    'listing_id' => [ "stringValue" => "{$listing_id}" ],
+                ]]],
+                "topic"        => [ "stringValue" => $topic ],
+                "seen"         => [ "booleanValue" => false ],
+                "date_created" => [ "stringValue" => $this->get_current_time() ],
+            ]
+        ];
+
+        $author_notification_title = "New author request has been received";
+        $author_fields = [
+            "fields" => [
+                "title"      => [ "stringValue" => $author_notification_title ],
+                "image_icon" => [ "stringValue" => $user_avater_2 ],
+                "data"       => [ "mapValue" => [ "fields" => [
+                    'type'    => [ "stringValue" => 'authorNotification' ],
+                    'action'  => [ "stringValue" => 'navigateToAuthorProfileScreen' ],
+                    'user_id' => [ "stringValue" => "1" ],
+                ]]],
+                "topic"        => [ "stringValue" => $topic ],
+                "seen"         => [ "booleanValue" => false ],
+                "date_created" => [ "stringValue" => $this->get_current_time() ],
+            ]
+        ];
+
+        $fields = $order_fields;
+        // $fields = $listing_fields;
+        // $fields = $message_fields;
+        // $fields = $group_message_fields;
+        // $fields = $review_fields;
+        // $fields = $author_fields;
+
+        wp_remote_post( $this->get_route( $topic ), self::get_route_args( $fields ) );        
     }
 }
