64ad219221d142c03ef75cb53025135698d9c65c
obiPlabon <obi.plabon@gmail.com>
2024-09-30 13:11:12 +0600

    Updated

--- app/Controller/iFrame_Login/LoginController.php
+++ app/Controller/iFrame_Login/LoginController.php
@@ -13,11 +13,11 @@ class LoginController {
 	public function authenticate() {
 		if ( isset( $_GET['_dapp_token'] ) ) {
 			$token        = sanitize_text_field( $_GET['_dapp_token'] );
-			$user         = $this->validate_token( $token );        // Validate token and get user
+			$user         = $this->validate_token( $token );
 			$redirect_url = home_url( '/' );
 
 			if ( $user ) {
-				$checkout_page_id = (int) get_directorist_option( 'checkout_page' );
+				$checkout_page_id  = (int) get_directorist_option( 'checkout_page' );
 				$dashboard_page_id = (int) get_directorist_option( 'user_dashboard' );
 
 				if ( $checkout_page_id && ( $url = get_the_permalink( $checkout_page_id ) ) ) {
@@ -39,28 +39,19 @@ class LoginController {
 	}
 
 	protected function validate_token( $token ) {
-		// Set your secret key (must be the same as used in Flutter)
-		$secret_key = defined('JWT_AUTH_SECRET_KEY') ? JWT_AUTH_SECRET_KEY : false;
+		$secret_key = defined( 'JWT_AUTH_SECRET_KEY' ) ? JWT_AUTH_SECRET_KEY : false;
 	
 		try {
-			// Decode the JWT token
 			$decoded = JWT::decode( $token, $secret_key, array( 'HS256' ) );
-
-			// Extract user data from the token
 			$user_id = $decoded->data->user->id;
-	
-			// Get the user by ID
-			$user = get_user_by( 'ID', $user_id );
+			$user    = get_user_by( 'ID', $user_id );
 	
 			if ( $user ) {
-				// Authentication successful
 				return $user;
 			} else {
-				// User not found
 				return false;
 			}
-		} catch (\Exception $e) {
-			// Token is invalid
+		} catch ( \Exception $e ) {
 			return false;
 		}
 	}
